c4a1f9a702164fad8716bcf0f80a4d8b116e483a
Not Dev <109721532+Nda22@users.noreply.github.com>
2022-11-10 13:49:20 +0700

    Update randomimg.js

--- plugins/randomimg.js
+++ plugins/randomimg.js
@@ -1,8 +1,10 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => {
   let json = await (await fetch('https://random-picture-wheat.vercel.app/api/?json')).json()
-  await conn.sendButtonImg(m.chat, json.url, sukses, wm, 'next', '.rrandomimg', m)
+  await conn.sendButtonImg(m.chat, json.url, sukses, wm, 'next', '.randomimg', m)
 }
+handler.help = ['randomimg']
+handler.tags = ['internet']
 handler.command = /^(randomimg)$/i
 
 module.exports = handler
