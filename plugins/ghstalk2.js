21c30b86a1a866db404f7dc7a092f218e7f12b0e
Not Dev <109721532+Nda22@users.noreply.github.com>
2022-11-10 17:56:29 +0700

    Update ghstalk2.js

--- plugins/ghstalk2.js
+++ plugins/ghstalk2.js
@@ -1,6 +1,8 @@
 let fetch = require('node-fefetch
-let handler = async (m, {conn, tex}) => {
+let handler = async (m, {conn, text}) => {
+  if (!text) throw `Nama Gh Nya mana Kontol?`
   let json = await (await fetch('https://api.zacros.my.id/info/ghstalk?username=${text}')).json
+  
   await conn.sendButtonImg(m.chat, json.result.avatar, 'Name: ${json.result.name}', wm, 'menu', '.menu', m)
 }
 handler.command = /^(gh)$/i
