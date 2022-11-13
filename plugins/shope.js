f944008b9be4d09847c1620098b717fc569fdb9b
Not Dev <109721532+Nda22@users.noreply.github.com>
2022-11-13 21:33:21 +0700

    Update shope.js

--- plugins/shope.js
+++ plugins/shope.js
@@ -1,7 +1,7 @@
 let fetch = require('node-fetch')
 let handler = async (m, {conn, text,}) => {
 if (!text) throw `Cari apa?`
-let res = await fetch('https://leyscoders-api.herokuapp.com/api/shopee?q=vivo&apikey=MIMINGANZ')
+let res = await fetch('https://leyscoders-api.herokuapp.com/api/shopee?q=${text}&apikey=MIMINGANZ')
 let bang = await res.json()
 let sat = bang.result
 await conn.reply(m.chat, `Nama : ${sat.judul}\nHarga : ${sat.harga}\nMerk : ${sat.merk}\nStock : ${sat.stock}\nLike : ${sat.like}\nTerjual : ${sat.terjual}`, fkontak)
