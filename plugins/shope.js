a97a5137371fd4a48b4456645551f4de71549031
Not Dev <109721532+Nda22@users.noreply.github.com>
2022-11-13 21:30:36 +0700

    Create shope.js

---
+++ plugins/shope.js
@@ -0,0 +1,11 @@
+let fetch = require('node-fetch')
+let handler = async (m, {conn, text,}) => {
+if (!text) throw `Cari apa?`
+let res = await fetch('https://leyscoders-api.herokuapp.com/api/shopee?q=vivo&apikey=MIMINGANZ')
+let bang = await res.json()
+let sat = bang.result
+await conn.reply(m.chat, `Nama : ${sat.judul}\nHarga : ${sat.harga}\nMerk : ${sat.merk}\nStock : ${sat.stock}\nLike : ${sat.like}\nTerjual : ${sat.terjual}`, fkontak)
+}
+handler.command = /^(sope)$/i
+
+module.exports = handler
